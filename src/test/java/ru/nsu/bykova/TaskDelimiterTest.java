7ebee29c8d6b8f09cad0d5b2b7282a3c2342510f
Galina Bykova <g.bykova@g.nsu.ru>
2024-02-06 15:06:07 +0700

    javadoc

--- src/test/java/ru/nsu/bykova/TaskDelimiterTest.java
+++ src/test/java/ru/nsu/bykova/TaskDelimiterTest.java
@@ -1,16 +1,13 @@
 package ru.nsu.bykova;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TaskDelimiterTest {
     @ParameterizedTest(name =
             "taskNumber = {0},zerothThreadsTaskNumber = {1}, firstThreadTaskNumber = {2}, secondThreadTaskNumber = {3}")
-    @CsvSource({
-            "9 , 3, 3, 3",
+    @CsvSource({"9 , 3, 3, 3",
             "3 , 1, 1, 1",
             "15, 5, 5, 5",
             "10, 4, 3, 3",
@@ -21,15 +18,14 @@ public class TaskDelimiterTest {
                              int zerothThreadsTaskNumber, int firstThreadTaskNumber, int secondThreadTaskNumber) {
         int threadNumber = 3;
         var taskDelimiter = new TaskDelimiter(taskNumber, threadNumber);
-        assertEquals(zerothThreadsTaskNumber, taskDelimiter.lenThreadPart(0));
-        assertEquals(firstThreadTaskNumber, taskDelimiter.lenThreadPart(1));
-        assertEquals(secondThreadTaskNumber, taskDelimiter.lenThreadPart(2));
+        Assertions.assertEquals(zerothThreadsTaskNumber, taskDelimiter.lenThreadPart(0));
+        Assertions.assertEquals(firstThreadTaskNumber, taskDelimiter.lenThreadPart(1));
+        Assertions.assertEquals(secondThreadTaskNumber, taskDelimiter.lenThreadPart(2));
     }
 
     @ParameterizedTest(name =
             "taskNumber = {0},zerothThreadsTaskNumber = {1}, firstThreadTaskNumber = {2}, secondThreadTaskNumber = {3}")
-    @CsvSource({
-            "9 , 0, 3, 6",
+    @CsvSource({"9 , 0, 3, 6",
             "3 , 0, 1, 2",
             "15, 0, 5, 10",
             "10, 0, 4, 7",
@@ -40,20 +36,18 @@ public class TaskDelimiterTest {
                  int zerothThreadsTaskNumber, int firstThreadTaskNumber, int secondThreadTaskNumber) {
         int threadNumber = 3;
         var taskDelimiter = new TaskDelimiter(taskNumber, threadNumber);
-        assertEquals(zerothThreadsTaskNumber, taskDelimiter.offsetThreadPart(0));
-        assertEquals(firstThreadTaskNumber, taskDelimiter.offsetThreadPart(1));
-        assertEquals(secondThreadTaskNumber, taskDelimiter.offsetThreadPart(2));
+        Assertions.assertEquals(zerothThreadsTaskNumber, taskDelimiter.offsetThreadPart(0));
+        Assertions.assertEquals(firstThreadTaskNumber, taskDelimiter.offsetThreadPart(1));
+        Assertions.assertEquals(secondThreadTaskNumber, taskDelimiter.offsetThreadPart(2));
     }
 
     @ParameterizedTest(name = "taskNumber = {0},threadNumber = {1}, threadIndex = {2}")
-    @CsvSource({
-            "1, 1, 1",
+    @CsvSource({"1, 1, 1",
             "1, 1, 3",
-            "1, 1, -5"
-    })
+            "1, 1, -5"})
     void invalidArgument(int taskNumber, int threadNumber, int threadIndex) {
         var taskDelimiter = new TaskDelimiter(taskNumber, threadNumber);
-        assertThrows(IllegalArgumentException.class, () -> taskDelimiter.lenThreadPart(threadIndex));
-        assertThrows(IllegalArgumentException.class, () -> taskDelimiter.offsetThreadPart(threadIndex));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> taskDelimiter.lenThreadPart(threadIndex));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> taskDelimiter.offsetThreadPart(threadIndex));
     }
 }
